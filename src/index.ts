d1203094ffbb6dce513c534ee08c5856e6716978
Samuel Gomes <samuel.gomes@onerf.com.br>
2024-09-22 21:00:22 -0300

    try vercel - test 1

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const session = require("express-session");
+const path = require("path");
+const flash = require("connect-flash");
+const bodyParser = require("body-parser");
+
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true,
+}));
+
+app.use(express.static(`${__dirname}/../public`));
+app.set("views", `${__dirname}/../views`);
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.use(flash());
+
+app.get("/", function(req, res) {
+    res.render("index", { layout: "base" });
+});
+
+const serve = app.listen(5000);
+console.log("Server is listening on port 5000");
